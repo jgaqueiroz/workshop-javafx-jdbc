3284ca03d3a05432467e7fb87195c21a6eec9637
José Queiroz <jqueiroz.av@gmail.com>
2025-07-11 20:26:42 -0300

    DepartmentService

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module br.com.effecta {
     requires javafx.fxml;
 
     opens br.com.effecta to javafx.fxml;
+    opens model.entities to javafx.base;
     opens gui to javafx.fxml;
 
     exports br.com.effecta;
