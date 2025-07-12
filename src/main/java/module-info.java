d44b4c715bc31f68c776a6533dfdd5b94a7c835a
José Queiroz <jqueiroz.av@gmail.com>
2025-07-11 22:18:42 -0300

    Adding database access

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module br.com.effecta {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens br.com.effecta to javafx.fxml;
     opens model.entities to javafx.base;
